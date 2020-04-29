58b971c9da351465c1cb8e02224ef82bf4920748
rociojoo <rocio.joo@ufl.edu>
2020-04-29 17:48:40 -0400

    Finished with code hmm1

--- Codes/hmms/1_run_simple_hmm_test_initial_values.R
+++ Codes/hmms/1_run_simple_hmm_test_initial_values.R
@@ -27,6 +27,7 @@ nlevels(sg$ID) # 38
 
 # ideally this script should be run on the full sample of individuals
 
+set.seed(1) # fixing the seed allows for comparable results, but you can remove this to make the sampling random
 ids <- sample(unique(sg$ID), 20, replace = FALSE)
 # create a new subset dataframe sg2
 sg2 <- subset(sg, ID %in% ids,)
@@ -55,16 +56,18 @@ hist(dat$angle)
 
 ##### 1D. TRY OUT SIMPLE HMM WITH 3 STATES #######
 
-
+# The order of these labels correspond to the one in our final results. You'll may have to alter it depending on your results.
 stateNames<-c("travel","search", "rest")
 
 
 # STEP LENGTH INITIAL VALUES #
 
-shape_0 <- c(15, 5, 1) # setting shape (i.e. means for the 3 states)
+shape_0 <- c(15, 5, 1) # setting shape (i.e. means for the 3 states; thinking in terms of travel, search and rest, in that order)
 scale_0 <- c(5, 4, 0.5)  # setting scale (i.e. variance)
 stepPar0 <- c(shape_0,scale_0)
 # artifact for zero step lengths for which there are only 2/24442 = <0.01% of values, so just set zero values to very small numbers
+# A proper way to deal with zeros is to use the zeromass parameters. 
+# As here there are only 2 zero values, this will not change the results
 ind_zero <- which(dat$step == 0)
 if (length(ind_zero)>0){
   dat$step[ind_zero] <- runif(length(ind_zero))/10000
@@ -92,18 +95,18 @@ m1 <- fitHMM(data=dat, nbStates=3,
              estAngleMean = list(angle=TRUE), # no need for fix par list beta as all transitions are possible. 
              stateNames = stateNames)
 print(m1) # model outputs 
-
+# 
 # step parameters:
 #   ----------------
-#         travel   search      rest
-# mean 18.728783 5.758392 0.6221451
-# sd    5.587003 6.474131 0.3246748
+#   travel   search      rest
+# mean 18.184348 5.295932 0.6253831
+# sd    5.873511 6.122766 0.3283630
 # 
 # angle parameters:
 #   -----------------
-#                   travel       search        rest
-# mean           0.002224857 -0.004025579  0.03487241
-# concentration 52.969930473  1.307708454 51.26332577
+#   travel      search        rest
+# mean           0.005895415 0.007634065  0.03301047
+# concentration 43.454467024 1.264779824 52.26784760
 
 # shows what model selects as "best" parameters for different states
 
@@ -141,6 +144,7 @@ search_angle <- expand.grid(Angle_loc_s =  seq(-0.2, 0.25, 0.05), Angle_con_s = 
 rest_angle <- expand.grid(Angle_loc_r =  seq(-0.2, 0.25, 0.05), Angle_con_r =  seq(10, 100, 10))
 
 # combine into dataframe and randomize order of all step and angle parameters
+# it will generate a "decent" set of combinations of randomized but realistic initial values for step and angle parameters
 all_vals <- cbind(travel_step[sample(nrow(travel_step)),], 
                   search_step[sample(nrow(search_step)),], 
                   rest_step[sample(nrow(rest_step)),], 
@@ -155,6 +159,7 @@ all_vals <- cbind(travel_step[sample(nrow(travel_step)),],
 # go through and each time choose a row of randomized values, run hmm and then output parameter values and AIC into dataframe
 # due to initial value combinations, sometimes the model cannot converge and so the tryCatch function is a workaround that allows you to skip
 # to the next combination. 
+# RJ: Is that why we get the warnings? If so, you should add a line about it.
 
 out.mod <- list()
 out.df <- list()
@@ -196,17 +201,22 @@ out.all <- do.call(rbind, out.df)
 out.all <- out.all[order(out.all$AIC),]
 
 # paste out
-out.path <- "./Data_outputs/Testing_initial_vals_SG.csv"
-write.csv(out.all, out.path, row.names=T)
+
+out.path <- "./Data_outputs/"
+if (dir.exists(out.path) == FALSE){
+  dir.create(out.path)
+}
+filename <- paste0(out.path,"Testing_initial_vals_SG.csv")
+write.csv(out.all, filename, row.names=T)
 
 
 head(out.all)
 # there are several cases within 2 units of the top AIC score
 # choosing just the "top" models that are within 2 AIC units of the highest value - as a fairly arbritary cut-off
-top <- subset(out.all, AIC < out.all$AIC[1]+2,)
+top <- subset(out.all, AIC < out.all$AIC[1]+2)
 nrow(top)
 
-# plotting histogram of step lenghts based on "best" models
+# plotting histogram of step lengths based on "best" models
 hist(top$T_step_m_OUT, breaks = 10)
 hist(top$S_step_m_OUT, breaks = 10)
 hist(top$R_step_m_OUT, breaks = 10)
@@ -214,20 +224,22 @@ hist(top$R_step_m_OUT, breaks = 10)
 # plotting step lengths together
 top.df <- data.frame(Vals = c(top$T_step_m_OUT, top$S_step_m_OUT, top$R_step_m_OUT),
                      State = c(rep("T", length(top$T_step_m_OUT)), rep("S", length(top$S_step_m_OUT)), rep("R", length(top$R_step_m_OUT))))
-ggplot(top.df, aes(Vals)) + geom_histogram(aes(fill = State))
+
+ggplot(top.df, aes(Vals)) + geom_histogram(aes(fill = State)) + xlab('Step length')
 # you can see that the "best" models consistently choose the same inital means for step length, but occassionally they are labelled in the wrong order
 # this is not problem - the purpose of this code is mainly to check that the model selects 3 peaks of parameter values. We know that of the 3 states
 # the model selects, from our knowledge of albatross ecology we know that travel will be the state with the greatest step lengths and rest the smallest. 
 
-# the values below are just for the subset (of 20 individuals)
+# the values below are just from the subset (of 20 individuals)
+# RJ: "average values?"
 # travel = 18.73
 # search = 5.59
 # rest = 0.36
 
 # plotting trips from "best" model to see if they make sense visually 
 
-head(out.all) # model number 79 was "best" for me, but will change each time as order will be randomized
-plot(out.mod[[79]])
+head(out.all) # model number 82 was "best" for me, but will change each time as order will be randomized
+plot(out.mod[[82]])
 # look good although states are labelled in the incorrect order. 
 
 
@@ -238,6 +250,7 @@ plot(out.mod[[79]])
 
 ########  2. CROZET ##########
 
+# This exactly the same analysis ran with the South Georgia dataset
 
 #### 2A.  LOAD IN DATA  ####
 
@@ -251,9 +264,11 @@ nlevels(cro$ID) # 267
 
 # ideally this script should be run on the full sample of individuals
 
+set.seed(2)
+
 ids <- sample(unique(cro$ID), 20, replace = FALSE)
 # create a new subset dataframe sg2
-cro2 <- subset(cro, ID %in% ids,)
+cro2 <- subset(cro, ID %in% ids)
 cro2$ID <- as.factor(as.character(cro2$ID))
 nlevels(cro2$ID) # 20
 
@@ -290,6 +305,7 @@ shape_0 <- c(12, 3, 1) # setting shape (i.e. means for the 3 states)
 scale_0 <- c(4, 3, 0.5)  # setting scale (i.e. variance)
 stepPar0 <- c(shape_0,scale_0)
 # artifact for zero step lengths for which there are only 124/232713 = 0.05% values, so just set zero values to small numbers
+# ideally use the zero mass parameter
 ind_zero <- which(dat$step == 0)
 if (length(ind_zero)>0){
   dat$step[ind_zero] <- runif(length(ind_zero))/10000
@@ -321,15 +337,15 @@ print(m1) # model outputs
 
 # step parameters:
 #   ----------------
-#         travel   search      rest
-# mean 12.46129 3.950403 0.3400040
-# sd    3.73476 4.442000 0.1949168
+#   travel   search      rest
+# mean 12.808237 4.384151 0.3195857
+# sd    3.497444 4.996611 0.1690926
 # 
 # angle parameters:
 #   -----------------
-#                   travel       search        rest
-# mean           0.003333838 -0.01591877  0.03050393
-# concentration 47.152833229  1.16361860 39.00367103
+#   travel     search        rest
+# mean           0.002588744 0.01749804  0.02294263
+# concentration 47.711634938 1.30130186 43.64997801
 
 # shows what model selects as "best" parameters for different states
 
@@ -422,14 +438,18 @@ out.all <- do.call(rbind, out.df)
 out.all <- out.all[order(out.all$AIC),]
 
 # paste out
-out.path <- "./Data_outputs/Testing_initial_vals_Cro.csv"
-write.csv(out.all, out.path, row.names=T)
+out.path <- "./Data_outputs/"
+if (dir.exists(out.path) == FALSE){
+  dir.create(out.path)
+}
+filename <- paste0(out.path,"Testing_initial_vals_Cro.csv")
+write.csv(out.all, filename, row.names=T)
 
 
 head(out.all)
 # there are several cases within 2 units of the top AIC score
 # choosing just the "top" models that are within 2 AIC units of the highest value - as a fairly arbritary cut-off
-top <- subset(out.all, AIC < out.all$AIC[1]+2,)
+top <- subset(out.all, AIC < out.all$AIC[1]+2)
 nrow(top)
 
 # plotting histogram of best values 
@@ -440,7 +460,8 @@ hist(top$R_step_m_OUT, breaks = 10)
 # plotting together
 top.df <- data.frame(Vals = c(top$T_step_m_OUT, top$S_step_m_OUT, top$R_step_m_OUT),
                      State = c(rep("T", length(top$T_step_m_OUT)), rep("S", length(top$S_step_m_OUT)), rep("R", length(top$R_step_m_OUT))))
-ggplot(top.df, aes(Vals)) + geom_histogram(aes(fill = State))
+
+ggplot(top.df, aes(Vals)) + geom_histogram(aes(fill = State)) + xlab('Step length')
 # you can see that the "best" models consistently choose the same inital means for step length, but occassionally they are labelled in the wrong order
 # this is not problem - the purpose of this code is mainly to check that the model selects 3 peaks of parameter values. We know that of the 3 states
 # the model selects, from our knowledge of albatross ecology we know that travel will be the state with the greatest step lengths and rest the smallest. 
@@ -452,8 +473,8 @@ ggplot(top.df, aes(Vals)) + geom_histogram(aes(fill = State))
 
 # plotting trips from "best" model to see if they make sense visually 
 
-head(out.all) # model number 8 was "best" for me, but will change each time as order will be randomized
-plot(out.mod[[8]])
+head(out.all) # model number 44 was "best" for me, but will change each time as order will be randomized
+plot(out.mod[[44]])
 # look good although states are labelled in the incorrect order. 
 
 
